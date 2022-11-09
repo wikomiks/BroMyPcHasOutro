27400c7cf65d40911567c7d8fb04bcef9e8e9d6a
wikomiks <97792998+wikomiks@users.noreply.github.com>
2022-11-09 21:27:38 +0100

    cool outro

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Media;
+using System.Diagnostics;
+
+namespace outro
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            SoundPlayer sound = new SoundPlayer("outro.wav");
+            sound.PlaySync();
+            Process.Start("shutdown.exe", "-s -t 00");
+        }
+    }
+}
